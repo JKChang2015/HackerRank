b12568f3bc0ad7c303eb8e071099f8593ce05f55
JKChang2015 <jkchang2015@gmail.com>
2018-08-14 14:42:15 +0100

    lists

---
+++ HackerP/Basic Data Types/Lists.py
@@ -0,0 +1,19 @@
+# Lists
+# Created by JKChang
+# 14/08/2018, 14:17
+# Tag:
+# Description: 
+
+if __name__ == '__main__':
+    cmd_count = int(input())
+    l = []
+
+    for _ in range(cmd_count):
+        s = input().split()
+        cmd = s[0]
+        args = s[1:]
+        if cmd != 'print':
+            cmd += "("+ ",".join(args) +")"
+            eval('l.' + cmd)
+        else:
+            print(l)

b947e9e01c1c08124cf2a6f2d084cb1a0f1731ac
JKChang2015 <jkchang2015@gmail.com>
2018-09-04 11:26:07 +0100

    regex

--- HackerJ/src/Strings/Java_Regex.java
+++ HackerJ/src/Strings/Java_Regex.java
@@ -32,6 +32,5 @@ class Java_Regex {
 }
 
 class MyRegex{
-    String pattern;
-
+    public String pattern = "((?:(?:25[0-5]|2[0-4]\\d|(?:1\\d{2}|[1-9]?\\d))\\.){3}(?:25[0-5]|2[0-4]\\d|(?:1\\d{2}|[1-9]?\\d)))";
 }

fb4d75d76fd20a060ac5660cc4f14de913b61614
JKChang2015 <jkchang2015@gmail.com>
2018-09-06 13:42:32 +0100

    test

--- HackerJ/src/test.java
+++ HackerJ/src/test.java
@@ -5,13 +5,29 @@
  * Description:
  */
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        String str = "This is an example string, right?  Yes!";
-        String delims = "[ .,?!]+";
-        String[] token = str.split(delims);
-        System.out.println(Arrays.toString(token));
+
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int n = in.nextInt();
+        int arr[] = new int[size];
+
+        for (int a_i = 0; a_i < size; a_i++) {
+            arr[a_i] = in.nextInt();
+        }
+
+        int[] rotated = new int[size];
+        for (int i = 0; i < size; i++) {
+            rotated[(i + size - n) % size] = arr[i];
+        }
+
+        for(int i=0; i < size; i++){
+            System.out.print(rotated[i] + " ");
+        }
+
+
     }
 }
